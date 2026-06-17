477896ac47b124e9f44dbffa79e2d9beb44c60f5
Sohom Chatterjee <sohom12345678910@gmail.com>
2026-06-17 13:19:47 +0530

    Implemented ProtectedRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import MyPastes from "./pages/MyPastes";
 import ViewPaste from "./pages/ViewPaste";
 import Navbar from"./components/Navbar";
 import { useNavigate } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+
 <components />
 function App() {
   
@@ -26,9 +28,9 @@ function App() {
 
         <Route path="/register" element={<Register />} />
 
-        <Route path="/create" element={<CreatePaste />} />
+        <Route path="/create" element={<ProtectedRoutes><CreatePaste /></ProtectedRoutes>} />
 
-        <Route path="/my-pastes" element={<MyPastes />} />
+        <Route path="/my-pastes" element={<ProtectedRoutes><MyPastes /></ProtectedRoutes>} />
 
         <Route path="/paste/:id" element={<ViewPaste />} />
 
